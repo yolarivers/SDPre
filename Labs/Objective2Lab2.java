301786c675b9c186c4cf58962c8a49ae412c478b
Yola Rivers <yolarivers@gmail.com>
2024-06-05 23:11:59 -0500

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Yola Rivers";
+    int age = 48;
+    String state = "California";
+
+    System.out.println("Name:" + " " + name + " " + "Age:" + " " + age + " " + "State:" + " " + state);
+    
+  }
+}
