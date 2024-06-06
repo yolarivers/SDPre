9b7fb3d20909283188fe4c5b981d73105c01f518
Yola Rivers <yolarivers@gmail.com>
2024-06-05 23:30:15 -0500

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    String month= "July";
+    int year = 1975;
+    boolean isWeekend = true;
+    boolean notWeekend = false;
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(notWeekend);
+  
+  }
+}
