7240776981134a593cb3e6867354e4beddf9aa20
Yola Rivers <yolarivers@gmail.com>
2024-06-06 12:59:01 -0500

    complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+  	double side1, side2, hypotenuse;
+    	side1 = 10.0;
+    	side2 = 8.0;
+    	hypotenuse = (Math.hypot(side1, side2));
+    	System.out.println(hypotenuse);
+
+  }
+}
