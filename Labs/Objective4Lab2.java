46ea41410b00a6adad3492b337e5163960a686c1
Yola Rivers <yolarivers@gmail.com>
2024-06-07 01:09:28 -0500

    complete Objective4Lab2

---
+++ Labs/Objective4Lab2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Objective4Lab2 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+
+   
+
+    System.out.println("Please enter the first whole number you would like to add.");
+    int num1 = keyboard.nextInt();
+    System.out.println("Please enter the second whole number you would like to add.");
+    int num2 = keyboard.nextInt();
+    System.out.println("Please enter the third whole number you would like to add.");
+    int num3 = keyboard.nextInt();
+    System.out.println("Please enter the first decimal number you would like to add.");
+    double dub1 = keyboard.nextDouble();
+    System.out.println("Please enter the second decimal number you would like to add.");
+    double dub2 = keyboard.nextDouble();
+    System.out.println("Please enter the third decimal number you would like to add.");
+    double dub3 = keyboard.nextDouble();
+
+
+    int numSum = num1 + num2 + num3;
+    double dubSum = dub1 + dub2 + dub3;
+
+     System.out.println("The sum of " + num1 + " + " + num2 + " + " + num3 + " = " + numSum + ".");
+
+     System.out.println("The sum of " + dub1 + " + " + dub2 + " + " + dub3 + " = " + dubSum + ".");
+
+
+
+  
+
+  }
+}
