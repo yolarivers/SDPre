55c7370d084aefe8d4ca59f625c281263864f9a6
Yola Rivers <yolarivers@gmail.com>
2024-06-07 02:10:53 -0500

    complete Objective4Lab3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+  public static void main(String[] args) {
+
+    
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old are you?");
+    int age = input.nextInt();
+    int currentYear = 2024;
+    int birthYear = currentYear - age;
+    System.out.println("You were born in " + birthYear);
+    input.close();
+  }
+}
