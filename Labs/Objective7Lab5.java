77bd065c9c4d190a9bae460bcf3a7d8d6df45642
Yola Rivers <yolarivers@gmail.com>
2024-06-08 01:34:46 -0500

    complete Objective7Lab5

---
+++ Labs/Objective7Lab5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Objective7Lab5 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while (true) {
+      System.out.println("_____Menu_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+      selection = scanner.nextInt();
+
+    
+      if (selection == 1) {
+        System.out.println("Hello World");
+      } else if (selection == 2) {
+        System.out.println("Apple, Banana, Coconut");
+      } else if (selection == 3) {
+        System.out.println("Goodbye");
+        break;
+    }
+    
+    }
+
+    scanner.close();
+  }
+}
