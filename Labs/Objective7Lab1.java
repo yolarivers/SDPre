b57d823076b3d74248253e1e3d4e068f2eca769c
Yola Rivers <yolarivers@gmail.com>
2024-06-07 22:47:47 -0500

    complete Objective7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,13 @@
+public class Objective7Lab1 {
+
+    public static void main(String[] args) {
+
+    int counter = 10;
+
+    while(counter >=1) {
+    System.out.println(counter);
+    counter--;
+
+        }
+  }
+}
