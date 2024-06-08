cedfdf409de54fdc2b2ecebb305382631ffd97eb
Yola Rivers <yolarivers@gmail.com>
2024-06-08 01:32:41 -0500

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+    public static void main(String[] args) {
+        int sum = 0;    
+        int count = 1; 
+
+        while (count <= 20) {
+            
+            sum += count;
+            count++;
+        }
+
+        
+        System.out.println( sum );
+    }
+}
