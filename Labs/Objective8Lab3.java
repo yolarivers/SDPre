5b2cd3850b8fdb52dcf21d969d82d6fad9ca5c85
Yola Rivers <yolarivers@gmail.com>
2024-06-08 14:19:43 -0500

    Objective8Lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,14 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+        
+        for (int counter = 1; counter <= 20; counter++) {
+            if (counter % 2 == 0) {
+              
+                System.out.println(counter + " is even");
+            } else {
+                
+                System.out.println(counter + " is odd");
+            }
+        }
+    }
+}
